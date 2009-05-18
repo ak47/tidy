0680de170b5fc5c969e823dfc1796c956450999c
Andy Koch <andy@pc-doctor.com>
2009-05-18 11:03:45 -0700

    ruby tidy updated with changes described by Karl Anderson here -- http://rubyforge.org/projects/tidy/

---
+++ lib/tidy/tidybuf.rb
@@ -0,0 +1,44 @@
+# Buffer structure.
+#
+class Tidybuf
+
+  extend DL::Importable
+  
+  # Access TidyBuffer instance.
+  #
+  attr_reader(:struct)
+
+  # Mimic TidyBuffer.
+  #
+  TidyBuffer = struct [
+    "TidyAllocator* allocator",
+    "byte* bp",
+    "uint size",
+    "uint allocated",
+    "uint next"
+  ]
+
+  def initialize
+    @struct = TidyBuffer.malloc
+    Tidylib.buf_init(@struct)
+  end
+    
+  # Free current contents and zero out.
+  #
+  def free
+    Tidylib.buf_free(@struct)
+  end
+
+  # Convert to array.
+  #
+  def to_a
+    to_s.split("\r\n")
+  end
+
+  # Convert to string.
+  #
+  def to_s
+    @struct.bp ? @struct.bp.to_s(@struct.size) : ''
+  end
+
+end
